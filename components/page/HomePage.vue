7533277525392b7a71a1cddbb666d41fa00200d6
ErenWeng <f245986@gmail.com>
2021-03-04 23:58:28 +0800

    feat: try api get data

--- components/page/HomePage.vue
+++ components/page/HomePage.vue
@@ -1,10 +1,22 @@
 <template>
-  <div class="home_page">home_page</div>
+  <div class="home_page">
+    <div v-for="(article, idx) in $store.state.articles" :key="idx" class="">
+      <div class="title">
+        {{ article.title }}
+      </div>
+      <div class="time">
+        {{ article.createdAt }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
+  created() {
+    this.$store.dispatch('getAllArticles')
+  },
 }
 </script>
 
--- store/index.js
+++ store/index.js
@@ -1,6 +1,7 @@
 export const state = () => ({
   toggleSidebar: false,
   toggleTopic: true,
+  articles: {},
 })
 
 export const mutations = {
@@ -10,6 +11,9 @@ export const mutations = {
   TOGGLE_TOPIC(state) {
     state.toggleTopic = !state.toggleTopic
   },
+  GET_All_ARTICLES(state, articles) {
+    state.articles = articles
+  },
 }
 
 export const actions = {
@@ -18,5 +22,32 @@ export const actions = {
   },
   toggleTopic({ commit }) {
     commit('TOGGLE_TOPIC')
+  },
+  async getAllArticles({ commit }) {
+    const basicArticlesUrl = 'http://localhost:3000/_content/articles'
+    const filterData = [
+      'title',
+      'description',
+      'img',
+      'slug',
+      'tags',
+      'createdAt',
+    ]
+    let query = ''
+    for (let i = 0; filterData.length > i; i++) {
+      query = '?only=' + filterData.join('&only=')
+    }
+    const articlesUrl = `${basicArticlesUrl + query}`
+
+    const res = await this.$axios.get(articlesUrl).catch((error) => {
+      /* eslint-disable no-console */
+      console.log(error)
+    })
+    const data = res.data
+
+    const dataSort = data.sort(function (a, b) {
+      return a.createdAt < b.createdAt ? 1 : -1
+    })
+    commit('GET_All_ARTICLES', dataSort)
   },
 }
