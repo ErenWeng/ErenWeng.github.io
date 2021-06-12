bb85c9b9836df343d59ea232e9a33aeb556ec0d1
ErenWeng <ErenWeng@users.noreply.github.com>
2021-06-12 12:04:27 +0000

    deploy: 3daff74fd2e65a94a02795ca700d4a6983aa8658

---
+++ _nuxt/static/1623499440/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623499440",layout:"default",error:b,state:{toggleSidebar:false,toggleTagList:a,tagsCount:[],isLoading:a,colorTheme:"light"},serverRendered:a,routePath:"\u002Fblog",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:b},content:{dbHash:"2d940b77"}}}}(true,null));
